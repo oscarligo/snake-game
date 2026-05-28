eb7aa7805e4edc9c8b750a9758c1a9bfc6929cd6
Oscar Rompich <oscarligo6@gmail.com>
2026-05-28 13:49:24 -0600

    feat: proyect's structure

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+const GRID_SIZE = 20; // 20x20 grid
+
+// snake starting position (3 segments)
+const INITIAL_SNAKE = [
+  { x: 10, y: 10 }, // Head
+  { x: 10, y: 11 }, // Body
+  { x: 10, y: 12 }  // Tail
+];
